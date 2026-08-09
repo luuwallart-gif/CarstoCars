0c96ea7906f4d6fcddc7fa367a857f8494e840ea
Meditora-digital <luuwallart@gmail.com>
2026-08-09 23:04:10 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'cc-bg': '#0a0e1a',
-        'cc-card': '#141b2e',
-        'cc-border': '#253150',
-        'cc-cyan': '#00d4ff',
-        'cc-red': '#e10600',
-        'cc-grey': '#8b9bb4',
+  'cc-bg': '#0a0e1a',
+  'cc-card': '#141b2e',
+  'cc-border': '#253150',
+  'cc-cyan': '#00d4ff',
+  'cc-red': '#e10600',
+  'cc-grey': '#8b9bb4',
+  'cc-grey2': '#5a6b8c',   // texte tertiaire
+  'cc-light': '#bbc9dd',   // texte clair
+  'cc-faint': '#3a4560',   // texte très effacé
       },
       fontFamily: {
         racing: ["'Racing Sans One'", 'cursive'],
